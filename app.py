6711de7b6e346e89b29adc5fd4af62e19c99d7c6
Aaryn Olsson <aaryno@gmail.com>
2023-04-22 06:18:17 +0000

    basic sql query working in flask app

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request
+import psycopg2, os
+
 app = Flask(__name__, static_url_path='/static')
+
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost',
+                            database='hawaii',
+                            user=os.environ['PGUSER'],
+                            password=os.environ['PGPASSWORD'])
+    return conn
 
 @app.route("/")
 def hello():
@@ -8,7 +17,27 @@ def hello():
 @app.route("/search")
 def search():
     place = request.args.get('place')
-    return ("<html><body>Search results for {place}</body></html>".format(place=place))
+    # place_like = "%{}%".format(place)
+    # return ("<html><body>Search results for {place}</body></html>".format(place=place))
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT osm_id, name, type, st_asgeojson(geometry) FROM import.osm_places WHERE upper(name) LIKE upper(%s);', [place])
+    places = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('results.html', places=places)
+
+
+@app.route('/searcg')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM places WHERE ;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', books=books)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
